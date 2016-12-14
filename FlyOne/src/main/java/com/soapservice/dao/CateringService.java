85a72f6de8761a4e5ffb35038a2696d20a284edf
sharlamov <syharlamov@gmail.com>
2016-12-14 16:01:16 +0200

    fix

--- FlyOne/src/main/java/com/soapservice/dao/CateringService.java
+++ FlyOne/src/main/java/com/soapservice/dao/CateringService.java
@@ -1,9 +1,12 @@
 package com.soapservice.dao;
 
+import com.soapservice.model.Item;
 import com.soapservice.model.Menu;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
 @Transactional
@@ -12,12 +15,15 @@ public class CateringService {
     @Autowired
     CateringDAO dao;
 
-    public Menu getMenu(){
+    public Menu getMenu() {
         Menu menu = new Menu();
         for (Object o : dao.getItems()) {
-            System.out.println(o);
+            Object[] matr = (Object[]) o;
+            Item item = new Item();
+            item.setId(((BigDecimal) matr[0]).intValue());
+            item.setCategoryId(((BigDecimal) matr[1]).intValue());
+            menu.getItem().add(item);
         }
-        //menu.setItem(dao.getItems());
         return menu;
     }
 }
---
+++ FlyOne/src/main/java/com/soapservice/endpoints/MsgHandler.java
@@ -0,0 +1,51 @@
+package com.soapservice.endpoints;
+
+import com.sun.xml.messaging.saaj.util.Base64;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class MsgHandler implements SOAPHandler {
+
+    private final String auth = "Basic " + new String(Base64.encode("fly:one".getBytes()));
+
+    @Override
+    public boolean handleMessage(MessageContext context) {
+        boolean result = true;
+        boolean isResponse = (Boolean) context.get(SOAPMessageContext.MESSAGE_OUTBOUND_PROPERTY);
+        if (!isResponse) {
+            System.out.println("Login");
+            Map http_headers = (Map) context.get(MessageContext.HTTP_REQUEST_HEADERS);
+            List userList = (List) http_headers.get("Authorization");
+            String username = null;
+            if (userList != null) {
+                username = userList.get(0).toString();
+            }
+            result = auth.equals(username);
+
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean handleFault(MessageContext context) {
+        return false;
+    }
+
+    @Override
+    public void close(MessageContext context) {
+    }
+
+
+    @Override
+    public Set<QName> getHeaders() {
+        return null;
+    }
+}
